776becc4c2cd0253bdfbcab30f217f95a6ddecae
nuhuxi <mark.keckeis@gmail.com>
2014-08-18 13:51:20 -0700

    first

---
+++ js/firstVideo.js
@@ -0,0 +1,12 @@
+/*var index = 1,
+      playlist = ['images/secondVideo.mp4'],
+      video = document.getElementById('videobcg');
+
+  video.addEventListener('ended', rotate_video, false);
+
+  function rotate_video() {
+    video.setAttribute('src', playlist[index]);
+    video.load();
+    index++;
+    if (index >= playlist.length) { index = 0; }
+  } */
